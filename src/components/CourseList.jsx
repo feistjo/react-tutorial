7ca5c742d97b7319c48f7d6e537197480de08a15
feistjo <joshua.feist@gmail.com>
2022-09-27 17:26:18 -0500

    Styling

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,6 +1,13 @@
+import './CourseList.css'
+
 const CourseList = ({courses}) => (
-    <div>
-        {Object.entries(courses).map(([_,course]) => <div>{course.term} CS {course.number}: {course.title}</div>)}
+    <div className="course-list">
+        {Object.entries(courses).map(([_,course]) => 
+        <div className="card m-1 p-2">
+            <h5 className='card-title'>{course.term} CS {course.number}</h5>
+            <p className='card-text'>{course.title}</p>
+            <p className='card-footer mt-auto'>{course.meets}</p>
+        </div>)}
     </div>
 );
 
